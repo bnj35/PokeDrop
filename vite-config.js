d866f95f64f115b821091dcfefb6cbff01c724cb
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-02-28 09:46:37 +0100

    Add Vite configuration and update HTML/CSS for info section

---
+++ vite-config.js
@@ -0,0 +1,37 @@
+
+import { resolve } from 'path'
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+
+export default {
+  root: './src/',
+  publicDir: '../public/',
+  base: './',
+  server:
+    {
+      host: true,
+      open: !isCodeSandbox // Open if it's not a CodeSandbox
+    },
+  build:
+    {
+      outDir: '../dist',
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions:{
+        input:{
+          main:resolve(__dirname,'src/index.html'),
+        },
+        output:{
+          manualChunks: {
+            three: ['three'],
+          },
+        }
+      }
+    },
+  resolve: {
+    alias: {
+      '@assets': '/assets',
+      '@js': '/js',
+      '@shaders': '/shaders',
+    }
+  }
+}
